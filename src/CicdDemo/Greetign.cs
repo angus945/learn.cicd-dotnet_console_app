c39404ce943aa93011d5c738827676e28b904951
angus945 main Desktop <angus9456@gmail.com>
2026-07-20 10:32:40 +0800

    chore: initialize .NET console app

---
+++ src/CicdDemo/Greetign.cs
@@ -0,0 +1,10 @@
+namespace CicdDemo
+{
+    public static class Greeting
+    {
+        public static string Create(string name)
+        {
+            return $"Hello, {name}! The CI/CD project is running.";
+        }
+    }
+}
---
+++ src/CicdDemo/Program.cs
@@ -0,0 +1,5 @@
+﻿using CicdDemo;
+
+var name = args.Length > 0 ? args[0] : "CI/CD Learner";
+
+Console.WriteLine(Greeting.Create(name));
